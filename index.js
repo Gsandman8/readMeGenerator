eb516d0b63b1dfca1c79d5a0b0277e4a081f6d7e
Gsandman8 <gage.e.sandman@gmail.com>
2023-10-03 20:32:27 -0700

    check for gitignore

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const generate = require("./utils/generateMarkdown.js");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -7,7 +9,13 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(
+        {type: "list",
+        name: "whatever",
+        choices: ["hey","whats up"]}
+    )
+}
 
 // Function call to initialize app
 init();
